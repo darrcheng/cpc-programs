f69377db651a6e27ab3f0c44032fc74e5b936af1
darrcheng <dhcheng@andrew.cmu.edu>
2024-03-27 13:20:02 -0400

    added data logging

--- Multi CPC Data Log/run_cpc.py
+++ Multi CPC Data Log/run_cpc.py
@@ -1,3 +1,5 @@
+import csv
+from datetime import datetime
 import numpy as np
 import os
 import queue
@@ -5,6 +7,7 @@ import threading
 import time
 import tkinter as tk
 from tkinter import ttk
+
 import yaml
 
 from cpcfnc import CPCSerial
@@ -41,12 +44,20 @@ class App:
             self.stop_threads,
             None,
         )
+        self.start_time, self.csv_filepath = self.create_files(
+            self.config["cpc1"]["cpc_header"]
+        )
         self.cpc.start()
 
         # Check the queue every 1s
         root.after(1000, self.check_queue)
 
     def check_queue(self):
+        # Create new file on new day
+        if datetime.now().day != self.start_time.day:
+            self.start_time, self.csv_filepath = self.create_files(
+                self.config["cpc1"]["cpc_header"]
+            )
         try:
             self.data = self.serial_queue.get_nowait()
             print(self.data)
@@ -54,6 +65,13 @@ class App:
         except queue.Empty:
             self.data = dict.fromkeys(self.config["cpc1"]["cpc_header"], np.nan)
             print(self.data)
+
+        all_values = [value for key, value in self.data.items()]
+
+        # Write all raw data to CSV file
+        with open(self.csv_filepath, mode="a", newline="") as data_file:
+            data_writer = csv.writer(data_file, delimiter=",")
+            data_writer.writerow(all_values)
 
         # if self.config["pulse_count"]:
         #     try:
@@ -74,13 +92,30 @@ class App:
         # except queue.Empty:
         #     pass
 
-        finally:
-            # Check the queue again after 1s
-            self.root.after(1000, self.check_queue)
+        # Check the queue again after 1s
+        self.root.after(1000, self.check_queue)
 
     def close(self):
         self.stop_threads.set()
         self.root.destroy()
+
+    def create_files(self, header):
+        start_time = datetime.now()
+        current_date = start_time.strftime("%Y-%m-%d")
+        subfolder_path = os.path.join(os.getcwd(), current_date)
+        os.makedirs(subfolder_path, exist_ok=True)
+
+        # Create CSV file and writer
+        file_datetime = start_time.strftime("%Y%m%d_%H%M%S")
+        csv_filename = "MANY_" + file_datetime + ".csv"
+        csv_filepath = os.path.join(subfolder_path, csv_filename)
+
+        # Open CSV logging file
+        with open(csv_filepath, mode="w", newline="") as data_file:
+            data_writer = csv.writer(data_file, delimiter=",")
+            data_writer.writerow(header)
+
+        return start_time, csv_filepath
 
 
 if __name__ == "__main__":
